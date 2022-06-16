668a948ebcc34e228ea65567d3c6fd7697a1d583
Fernando Silva <fernandonsilva16@gmail.com>
2022-06-16 13:21:00 -0300

    feat(sum): sum (#2)

--- src/Peano.lean
+++ src/Peano.lean
@@ -1,1 +1,9 @@
-#eval "Hello, World!"
+inductive ℕ where
+  | zero : ℕ
+  | succ : ℕ -> ℕ
+  deriving Repr
+
+def sum (x y : ℕ) : ℕ :=
+  match x with
+    | ℕ.zero => y
+    | ℕ.succ x' => sum x' (ℕ.succ y)
