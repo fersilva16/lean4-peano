17e959d3cb215e7b2180eccbf2e7e15bede8432e
Fernando Silva <fernandonsilva16@gmail.com>
2022-11-19 02:12:48 -0300

    feat(notation): improve examples (#14)

--- src/Peano.lean
+++ src/Peano.lean
@@ -82,14 +82,15 @@ infix:50 " <' " => ℕ.lt
 infix:50 " <=' " => ℕ.lte
 infix:50 " ==' " => ℕ.eq
 
-#eval ℕ.succ ℕ.zero +' ℕ.succ ℕ.zero
-#eval ℕ.succ ℕ.zero +' ℕ.succ ℕ.zero -' ℕ.succ ℕ.zero
-#eval ℕ.succ (ℕ.succ ℕ.zero) -' ℕ.succ ℕ.zero
-#eval ℕ.succ (ℕ.succ ℕ.zero) *' ℕ.succ (ℕ.succ ℕ.zero)
-#eval ℕ.succ (ℕ.succ ℕ.zero) /' ℕ.succ ℕ.zero
-#eval ℕ.succ (ℕ.succ ℕ.zero) >' ℕ.succ ℕ.zero
-#eval ℕ.succ (ℕ.succ ℕ.zero) >=' ℕ.succ (ℕ.succ ℕ.zero)
-#eval ℕ.succ ℕ.zero <' ℕ.succ (ℕ.succ ℕ.zero)
-#eval ℕ.succ (ℕ.succ ℕ.zero) <=' ℕ.succ (ℕ.succ ℕ.zero)
-#eval ℕ.succ (ℕ.succ ℕ.zero) ==' ℕ.succ (ℕ.succ ℕ.zero)
-#eval ℕ.succ (ℕ.succ ℕ.zero) ==' 2
+#eval 1 +' 1       -- 2
+#eval 2 -' 1       -- 1
+#eval 2 *' 2       -- 4
+#eval 6 /' 2       -- 3
+#eval 2 >' 1       -- true
+#eval 2 >=' 2      -- true
+#eval 1 <' 2       -- true
+#eval 2 <=' 2      -- true
+#eval 2 ==' 2      -- true
+#eval 2 +' 4 -' 1  -- 5
+#eval 10 *' 3 /' 2 -- 15
+#eval 1 +' 2 ==' 3 -- true
